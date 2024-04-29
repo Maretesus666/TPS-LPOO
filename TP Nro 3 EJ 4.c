f14af250bb14626f268dc4a9105a1edb35e787d7
Maretesus666 <166169222+Maretesus666@users.noreply.github.com>
2024-04-29 15:34:03 -0300

    Update TP Nro 3 EJ 4.c

--- TP Nro 3 EJ 4.c
+++ TP Nro 3 EJ 4.c
@@ -10,7 +10,13 @@ void copiarArreglo(int *origen, int *destino, int size) {
 }
 
 int main() {
-    
+    int arreglo_origen[] = {10, 20, 30, 40, 50}; 
+    int size = sizeof(arreglo_origen) / sizeof(arreglo_origen[0]);
+    int arreglo_destino[size];
+
+    copiarArreglo(arreglo_origen, arreglo_destino, size);
+
+    printf("Arreglo original: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", arreglo_origen[i]); 
     }
@@ -22,4 +28,5 @@ int main() {
 
     return 0;
 }
+
 
